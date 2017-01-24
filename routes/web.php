5c8561bc8a33d4beaf63d33366738031176846a7
Youghourta Benali <youghourta@gmail.com>
2017-01-24 16:57:57 +0100

    clean the route file

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+/* general purpose routes*/
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', 'HomeController@index');
+
+
+/* twitter relate routes*/
 Route::get('twitter', ['as' => 'twitter', 'uses' => 'TwitterController@index']);
 Route::get('twitter/login', ['as' => 'twitter.login', 'uses' => 'TwitterController@getLogin']);
 Route::get('twitter/callback', ['as' => 'twitter.callback', 'uses' => 'TwitterController@getCallback']);
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index');
-
+/* profile relate routes*/
 Route::get('profile', ['as' => 'profile.show', 'uses' => 'UserProfileController@show']);
 Route::get('profile/create', ['as' => 'profile.create', 'uses' => 'UserProfileController@create']);
 Route::post('profile/store', ['as' => 'profile.store', 'uses' => 'UserProfileController@store']);
