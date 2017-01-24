31dc69cbdeaecdc7a617c3068157ded7694c85cb
Youghourta Benali <youghourta@gmail.com>
2017-01-24 09:14:26 +0100

    add the routes necessary for the profile

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('twitter/callback', ['as' => 'twitter.callback', 'uses' => 'TwitterCo
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('profile', ['as' => 'profile.show', 'uses' => 'UserProfileController@show']);
+Route::get('profile/create', ['as' => 'profile.create', 'uses' => 'UserProfileController@create']);
+Route::post('profile/store', ['as' => 'profile.store', 'uses' => 'UserProfileController@store']);
