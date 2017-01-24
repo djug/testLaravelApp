e50eb957346e9b87c5b1665d25442f007f94c911
Youghourta Benali <youghourta@gmail.com>
2017-01-24 08:20:17 +0100

    add the fillable fields to the TwitterAccount model

--- app/TwitterAccount.php
+++ app/TwitterAccount.php
@@ -7,7 +7,7 @@ use App\User;
 
 class TwitterAccount extends Model
 {
-
+    protected $fillable = ['twitter_screen_name', 'twitter_user_id', 'oauth_token', 'oauth_token_secret', 'user_id'];
     public function user()
     {
         return $this->belongsTo(User::class);
