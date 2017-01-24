49ab7f9f6adbf4fa0020eae82d62233a331e3b92
Youghourta Benali <youghourta@gmail.com>
2017-01-24 08:02:10 +0100

    add the twitterAccount model and migration

---
+++ database/migrations/2017_01_24_060942_create_twitter_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTwitterAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('twitter_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('twitter_screen_name');
+            $table->string('twitter_user_id');
+            $table->string('oauth_token');
+            $table->string('oauth_token_secret');
+            $table->integer('user_id')->unsigned();
+                $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('twitter_accounts');
+    }
+}
